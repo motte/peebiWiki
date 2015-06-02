d427f49e7f349b726741d52f91370a6cce49343a
Michael Otte <michaelotte1@gmail.com>
2015-06-01 23:46:08 -0700

    Initial

---
+++ peebiwiki/tiddlywebconfig.py
@@ -0,0 +1,8 @@
+# A basic configuration.
+# `pydoc tiddlyweb.config` for details on configuration items.
+
+config = {
+    'system_plugins': ['tiddlywebwiki'],
+    'secret': '7a9f9202abc2cd63bb39df3efd0a669a1a3059a6',
+    'twanager_plugins': ['tiddlywebwiki'],
+}
